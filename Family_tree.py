65a534da928c211a425c2267bfe90d9b207c558c
Yana Dieringer <yana.dieringer@nordstrom.com>
2020-10-26 14:51:24 -0700

    Changes as of 26 October 2020

--- Family_tree.py
+++ Family_tree.py
@@ -1,3 +1,4 @@
+#create class
 class Member():
 
         def __init__(self):
@@ -18,115 +19,90 @@ class Member():
             if self.children == None:
                 self.children = []
             self.children.append(member)
+            
 
-
-
-def create_member(member, file, code):
+#read csv file
+def read_file(file, code):
     import csv
     data = open (file, encoding = code)
     csv_data = csv.reader (data)
-    data_lines = list(csv_data)
+    all_rows = list(csv_data)
+    member_list = [] 
+    
+    for row in all_rows[1:]:  
+        member = Member()
+        member.name = row[1]
+        member.index = row[2]
+        member.last_name = row[3]      
+        member.gender = row[4]
+        member.mother = row[5]
+        member.father = row[6]
+        member_list.append(member)
+        if row[9] == "TRUE":
+            root = member
+            
+    for index, member in enumerate(member_list):
 
-    for line in data_lines[1:]:  
-        if member == line [0]: 
-            member = Member()
-            member.name = line[1]
-            member.index = line[2]
-            member.last_name = line[3]
-            member.gender = line[4]
-            member.mother = line[5]
-            member.father = line[6]
-            member.spouse = line[7]
-            member.children = line[8]
-    return (member)
+        for each in member_list:            
+            if each.mother == member_list[index].index:
+                each.mother = member_list[index]
 
 
-def printInorder(member, indentation):
-    member.left = member.mother
-    member.right = member.father
+            if each.father == member_list[index].index:
+                each.father = member_list[index]
+    print (f"The root of the family is {root}.")
+    return (root)
 
-    if member.left=="Null" or member.right =="Null":
-        print ("                                      " + f"{member}")
+
+# print family tree
+
+def print_family(root, indentation):
+    root.left = root.mother
+    root.right = root.father
+ 
+
+    if root.left=="Null" or root.right =="Null":
+        print ("                                              " + f"{root}")
         return
     else: 
-        indentation += "        "
-        # First recur on left child 
-        printInorder(member.left, indentation) 
-
+        indentation += "           "
+        
+        # first recur on left child 
+        print_family(root.left, indentation) 
+        
+        
         # then print the data of node 
-        print ("")
-        print(indentation + f"{member}"),
+
+        print ("  ")
+        print(f"{indentation}"  + f"{root}")
+        print ("  ")
+
 
         # now recur on right child 
-        printInorder(member.right, indentation) 
+        print_family(root.right, indentation) 
+        
 
+# find any member in the tree
 
-
-
-def printPostorder(member, indentation): 
-    member.left = member.mother
-    member.right = member.father
-
-    if member.left== None or member.right ==None:
-        print ("                                      " + f"{member}")
+def find_member (root, name):
+    
+    if root == "Null":
         return
+    
+    if root.name == name:
+        member = root
+        print_family(member, "")
+        return member
 
-    else:
+    find_member(root.left, name)
 
-        indentation += "        "
-        # First recur on left child 
-        printPostorder(member.left, indentation) 
-  
-        # the recur on right child 
-        printPostorder(member.right, indentation) 
-  
-        # now print the data of node 
-        print ("")
-        print(indentation + f"{member}"),
+    find_member(root.right, name)
 
-Feder = create_member("Feder", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Lukeria = create_member("Lukeria", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Maria = create_member("Maria", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Grigoriy = create_member("Grigoriy", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Lubov = create_member("Lubov", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Bogdan = create_member("Bogdan", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Roman = create_member("Roman", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Yana = create_member("Yana", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Fanie = create_member("Fanie", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Albert1 = create_member("Albert1", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Margaret1 = create_member("Margaret1", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Albert2 = create_member("Albert2", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Margaret = create_member("Margaret", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Alan = create_member("Alan", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Ambrose = create_member("Ambrose", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
-Albert = create_member("Albert", "/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
 
-Lukeria.spouse = Feder
-Feder.spouse = Lukeria
-Grigoriy.spouse = Maria
-Maria.spouse = Grigoriy
-Lubov.mother = Lukeria
-Lubov.father = Feder
-Bogdan.mother = Maria
-Bogdan.father = Grigoriy
-Bogdan.spouse = Lubov
-Lubov.spouse = Bogdan
-Roman.father = Bogdan
-Roman.mother = Lubov
-Yana.father = Bogdan
-Yana.mother = Lubov
-Albert1.spouse = Fanie
-Fanie.spouse = Albert1
-Margaret1.spouse = Albert2
-Alan.mother = Margaret1
-Alan.father = Albert2
-Margaret.mother = Fanie
-Margaret.father = Albert1
-Alan.spouse = Margaret
-Ambrose.mother = Margaret
-Ambrose.father = Alan
-Ambrose.spouse = Yana
-Yana.spouse = Ambrose
-Albert.mother = Yana
-Albert.father = Ambrose
-printInorder(Albert, " ")
+#Test
+read_file ("/Users/ef5j/git_repo/Members_Information.csv", "utf-8-sig")
+
+print_family(root, "")
+print ("_________________________________________________________________________")
+
+find_member(root, "Ambrose")
